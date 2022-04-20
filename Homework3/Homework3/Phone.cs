8a4af0c39013c88fcf893fc09322f2b4041dc14f
izveginceva <ilga.zveginceva@gmail.com>
2022-04-20 22:23:36 +0300

    First excercise done

---
+++ Homework3/Homework3/Phone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public class Phone
+
+    {
+        public string Size { get; set; }
+        public string Model { get; set; }
+        public string Mark { get; set; }
+
+        public void Call()
+        {
+
+        }
+
+        public void SendSms()
+        {
+
+        }
+
+    }
+}
--- Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Program.cs
@@ -1,1 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Homework3;
+
+Phone samsung = new Phone();
+
+Console.WriteLine($"Enter phone mark, size, model");
+
+string mark = Console.ReadLine();
+string size = Console.ReadLine();
+string model = Console.ReadLine();
+
+samsung.Mark = mark;
+samsung.Size = size;    
+samsung.Model = model;
+
+Console.WriteLine($"Your phone mark is {samsung.Mark}, your phone size is {samsung.Size}, your phone model is {samsung.Model}");
+
